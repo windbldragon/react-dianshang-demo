d9361e0f7609d56eefc3e681ec7877a3062da762
bailong <372393485@qq.com>
2017-08-04 10:33:21 +0800

    test

--- app/containers/Home/index.jsx
+++ app/containers/Home/index.jsx
@@ -26,6 +26,7 @@ class Home extends React.Component {
                 {/*<SelectDemo/>*/}
                 <div style={{height: '15px'}}></div>
                 <Ad/>
+                <div>test</div>
                 <div style={{height: '15px'}}></div>
                 <Recommend/>
             </div>
