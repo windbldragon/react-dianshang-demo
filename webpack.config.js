e98746ebd102b58d9ec0f77265d24104cf9e238a
bailong <372393485@qq.com>
2017-08-06 17:40:04 +0800

    更改了webpack配置

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,20 @@ var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 // console.log(process.env.NODE_ENV)
 
 module.exports = {
+    devtool: 'eval-source-map',
     entry: path.resolve(__dirname, 'app/index.jsx'),
     output: {
         path: __dirname + "/build",
-        filename: "bundle.js"
+        filename: "bundle-[hash].js"
     },
 
     resolve:{
+        //一个包含模块扩展名的数组。例如，为了发现CoffeeScript 文件，你的数组应该包含字符串".coffee"。
+        //Default: ["", ".webpack.js", ".web.js", ".js"]
+        //注意：设置了这个选项，会取代默认的模块扩展名。重要的事情说三遍：意味着webpack不再用默认扩展名查找模块，
+        // 意味着webpack不再用默认扩展名查找模块，意味着webpack不再用默认扩展名查找模块。
+        // 如果你想正确加载一个带有扩展名的模块，你必须把一个空字符串放在你的数组里。
+        // 如果你想不要扩展名来加载一个js文件，你需要将“.js”加入你的数组。
         extensions:['', '.js','.jsx']
     },
 
@@ -23,6 +30,7 @@ module.exports = {
         //     // 报错 ？？？？？
         //     {test: /\.(js|jsx)$/, loader: "eslint-loader", exclude: /node_modules/}
         // ],
+        //postcss是为了给css添加不同的前缀以用来兼容浏览器，就是下面的autoprefixer
         loaders: [
             { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel' },
             { test: /\.less$/, exclude: /node_modules/, loader: 'style!css!postcss!less' },
@@ -31,13 +39,13 @@ module.exports = {
             { test:/\.(png|woff|woff2|svg|ttf|eot)($|\?)/i, loader:'url-loader?limit=5000'} // 限制大小小于5k
         ]
     },
-
+    //这个是校验代码规范的，配置在.eslintrc中
     eslint: {
         configFile: '.eslintrc' // Rules for eslint
     },
 
     postcss: [
-        require('autoprefixer') //调用autoprefixer插件，例如 display: flex
+        require('autoprefixer') //调用autoprefixer插件，例如 display: flex  给不同浏览器加前缀，也就是浏览器兼容性的问题
     ],
 
     plugins: [
@@ -73,6 +81,6 @@ module.exports = {
         colors: true, //终端中输出结果为彩色
         historyApiFallback: true, //不跳转
         inline: true, //实时刷新
-        hot: true  // 使用热加载插件 HotModuleReplacementPlugin
+        hot: true,  // 使用热加载插件 HotModuleReplacementPlugin
     }
 }
